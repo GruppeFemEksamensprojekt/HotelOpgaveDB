59df8760ac1ce3549afb970c654582efdc7a6352
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-03-31 12:18:32 +0200

    fgregret

---
+++ HotelLorteOpgave/HotelLorteOpgave/Viewmodel/HotelViewmodel.cs
@@ -0,0 +1,32 @@
+﻿using HotelLorteOpgave.Persistancy;
+using HotelLorteOpgaveWebservice;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLorteOpgave.Viewmodel
+{
+
+   public class HotelViewmodel
+    {
+        public HotelViewmodel()
+        {
+            Hotels = new ObservableCollection<Hotel>();
+            LoadHotelsAsync();
+        }
+        public ObservableCollection<Hotel> Hotels { get; set; }
+
+        public async void LoadHotelsAsync()
+        {
+            Hotels = await PersistanceService.LoadHotelsFromJsonAsync();
+
+                foreach (var hotel in Hotels)
+                {
+                    Hotels.Add(hotel);
+                }                     
+        }
+    }
+}
